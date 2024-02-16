04651131febee04a8bd693d80ae84a287a11665a
abdulra <abdulra@ABDULRA-IN-LE01>
2024-02-16 12:36:03 +0530

    Add project files.

---
+++ StudentInfo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentInfo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ StudentInfo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentInfo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
